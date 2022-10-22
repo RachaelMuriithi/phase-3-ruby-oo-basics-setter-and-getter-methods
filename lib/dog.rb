2492a805eb5b877a6b39f4714128b70cd7da01a7
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-22 10:02:27 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
-
-end
+    # setter for the name of the dog
+    def name=(name)
+      @name = name
+    end
+  
+    # setter for the breed  of the dog
+    def breed=(breed)
+      @breed = breed
+    end
+  
+    # getter for the name of the dog
+    def name
+      @name
+    end
+  
+    # getter for the name of the dog
+    def breed
+      @breed
+    end
+  end
